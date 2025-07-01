bc597bcd70255c781e71c372749aeba66a26fc84
ganesh avireddy <ganeshavireddy@gmail.com>
2025-07-01 19:04:29 +0530

    source code

---
+++ src/Components/Testimonial.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ProfilePic from "../Assets/john-doe-image.png";
+import { AiFillStar } from "react-icons/ai";
+
+const Testimonial = () => {
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <p className="primary-subheading">Testimonial</p>
+        <h1 className="primary-heading">What They Are Saying</h1>
+        <p className="primary-text">
+         Our experience was amazing — the food was fresh, flavorful, and delivered right on time. The entire process was so smooth, from ordering to delivery.
+        </p>
+      </div>
+      <div className="testimonial-section-bottom">
+        <img src={ProfilePic} alt="" />
+        <p>
+          “The meals are absolutely delicious and always arrive hot! This service has made dinner time stress-free and enjoyable for my whole family.”
+        </p>
+        <div className="testimonials-stars-container">
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+        </div>
+        <h2>Ganesh avireddy</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
---
+++ src/Components/Work.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PickMeals from "../Assets/pick-meals-image.png";
+import ChooseMeals from "../Assets/choose-image.png";
+import DeliveryMeals from "../Assets/delivery-image.png";
+
+const Work = () => {
+  const workInfoData = [
+    {
+      image: PickMeals,
+      title: "Pick Meals",
+      text: "Browse our menu and pick your favorite meals made with fresh ingredients.",
+    },
+    {
+      image: ChooseMeals,
+      title: "Choose How Often",
+      text: "Select how often you want your meals — daily, weekly, or just once!",
+    },
+    {
+      image: DeliveryMeals,
+      title: "Fast Deliveries",
+      text: "Enjoy lightning-fast delivery, bringing hot meals right to your doorstep",
+    },
+  ];
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <p className="primary-subheading">Work</p>
+        <h1 className="primary-heading">How It Works</h1>
+        <p className="primary-text">
+          Explore our delicious menu, place your order with ease, and enjoy fresh, hot meals delivered right to your doorstep.
+        </p>
+      </div>
+      <div className="work-section-bottom">
+        {workInfoData.map((data) => (
+          <div className="work-section-info" key={data.title}>
+            <div className="info-boxes-img-container">
+              <img src={data.image} alt="" />
+            </div>
+            <h2>{data.title}</h2>
+            <p>{data.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Work;
